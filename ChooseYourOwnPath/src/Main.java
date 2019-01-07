caee6eab39ecf1e71d31cbe8270ebd88040539b9
ImFrog <22986484+ImFrog@users.noreply.github.com>
2019-01-07 13:39:15 -0800

    something

--- ChooseYourOwnPath/src/Main.java
+++ ChooseYourOwnPath/src/Main.java
@@ -1,8 +1,14 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
+        int numPages = scan.nextInt();
+
+        String[][] input = new String[numPages][];
+        for(int i=0;i<numPages;i++) {
+                input[i] = scan.nextLine().split(" ");
+        }
     }
 }
