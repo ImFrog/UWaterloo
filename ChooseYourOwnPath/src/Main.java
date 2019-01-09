8c366e3ca70888d4f77ec860372c560931539d5c
ImFrog <22986484+ImFrog@users.noreply.github.com>
2019-01-09 13:15:24 -0800

    update

--- ChooseYourOwnPath/src/Main.java
+++ ChooseYourOwnPath/src/Main.java
@@ -4,11 +4,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int numPages = scan.nextInt();
+        int numPages = Integer.parseInt(scan.nextLine());
 
-        String[][] input = new String[numPages][];
-        for(int i=0;i<numPages;i++) {
-                input[i] = scan.nextLine().split(" ");
+        int[][] input = new int[numPages][];
+        for(int i=0;i<input.length;i++) {
+            String[] nextLine = scan.nextLine().split(" ");
+            int[] pages = new int[nextLine.length];
+            for(int k=0;k<nextLine.length;k++) {
+                pages[k] = Integer.parseInt(nextLine[k]);
+            }
+            input[i] = pages;
+        }
+
+
+        for(int[] i: input) {
+            for(int j: i) {
+                if (j == 0) {
+
+                }
+            }
         }
     }
 }
