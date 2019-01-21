5949f0cf18d599ddd3a8e47588a8265af428dc3d
ImFrog <22986484+ImFrog@users.noreply.github.com>
2019-01-21 13:40:39 -0800

    ChooseYourOwnPath

--- ChooseYourOwnPath/src/Main.java
+++ ChooseYourOwnPath/src/Main.java
@@ -1,28 +1,35 @@
-import java.lang.reflect.Array;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+    private static int[][] pages;
+
+    private static int walkThroughAllPaths(int[] pageData) {
+        int num = 0;
+        for(int i=1;i<pageData.length;i++) {
+            int temp = walkThroughAllPaths(pages[pageData[i]]);
+            if (temp > num) num = temp;
+        }
+        if(pageData[0] == 0) return 1;
+        else num += 1;
+        return num;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scan = new Scanner(new File("H:\\My Documents\\Programming\\Programming 12\\UWaterloo\\ChooseYourOwnPath\\src\\in"));
         int numPages = Integer.parseInt(scan.nextLine());
+        pages = new int[numPages][];
 
-        int[][] input = new int[numPages][];
-        for(int i=0;i<input.length;i++) {
+        for(int i=0;i<pages.length;i++) {
             String[] nextLine = scan.nextLine().split(" ");
-            int[] pages = new int[nextLine.length];
+            int[] pageData = new int[nextLine.length];
             for(int k=0;k<nextLine.length;k++) {
-                pages[k] = Integer.parseInt(nextLine[k]);
+                pageData[k] = Integer.parseInt(nextLine[k]);
             }
-            input[i] = pages;
+            pages[i] = pageData;
         }
 
-
-        for(int[] i: input) {
-            for(int j: i) {
-                if (j == 0) {
-
-                }
-            }
-        }
+        System.out.println("\n" + walkThroughAllPaths(pages[0]));
     }
 }
