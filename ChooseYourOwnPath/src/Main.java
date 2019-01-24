570d7d0be1d4674c30c99c955a87c8bd507702ed
ImFrog <22986484+ImFrog@users.noreply.github.com>
2019-01-24 00:21:50 -0800

    restart

--- ChooseYourOwnPath/src/Main.java
+++ ChooseYourOwnPath/src/Main.java
@@ -1,22 +1,8 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
     private static int[][] pages;
-    private static int[][] pagesCopy;
-
-    private static int walkThroughAllPaths(int[] pageData) {
-        int num = 999999999;
-        for(int i=1;i<pageData.length;i++) {
-            pagesCopy[pageData[i] - 1] = null;
-            int temp = walkThroughAllPaths(pages[pageData[i] - 1]);
-            if (temp < num) num = temp;
-        }
-        if(pageData[0] == 0) return 1;
-        else num += 1;
-        return num;
-    }
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scan = new Scanner(System.in);
@@ -32,21 +18,5 @@ public class Main {
             pages[i] = pageData;
         }
 
-        pagesCopy = pages.clone();
-        pagesCopy[0] = null;
-
-        int shortestPathNum = walkThroughAllPaths(pages[0]);
-
-        boolean reachesAllpages = true;
-        for(int[] page:pagesCopy) {
-            if(page != null) {
-                reachesAllpages = false;
-                break;
-            }
-        }
-
-        if(reachesAllpages) System.out.println("Y");
-        else System.out.println("N");
-        System.out.println(shortestPathNum);
     }
 }
