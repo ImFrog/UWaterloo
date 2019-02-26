f867658f919d6a040af4f9934d4bb01839913d0f
ImFrog <22986484+ImFrog@users.noreply.github.com>
2019-02-26 11:24:24 -0800

    VoronoiVillages

---
+++ VoronoiVillages/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] villages = new int[scan.nextInt()];
+
+        for(int i = 0; i < villages.length; i++) {
+            villages[i] = scan.nextInt();
+        }
+
+        Arrays.sort(villages);
+
+        for (int i: villages) {
+            System.out.println(i);
+        }
+
+        double size = 0;
+
+        for(int i = 1; i < villages.length - 1; i++) {
+            double neighborhoodSize = (villages[i] - villages[i-1]) / 2.0 + (villages[i + 1] - villages[i]) / 2.0;
+
+            if (size == 0) {
+                size = neighborhoodSize;
+                continue;
+            }
+
+            if (neighborhoodSize < size) size = neighborhoodSize;
+        }
+
+        System.out.println(size);
+    }
+}
