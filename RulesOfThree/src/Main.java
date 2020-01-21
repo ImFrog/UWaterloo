a686beb85451a02a2d60cc616fb52fc0c7c56d4e
ImFrog <22986484+ImFrog@users.noreply.github.com>
2020-01-21 09:15:40 -0800

    Update 1/21/2020

--- RulesOfThree/src/Main.java
+++ RulesOfThree/src/Main.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     private static String[][] rules = new String[3][2];
+    private static ArrayList<String> output = new ArrayList<>();
 
     private static boolean something(String start, String goal, int step) {
+        if (step > 4) return false;
         if (start.equals(goal)) return true;
         for (int i = 0; i < rules.length; i++) {
             if (start.contains(rules[i][0])) {
@@ -14,13 +17,12 @@ public class Main {
                 while (temp.contains(rules[i][0])) {
                     begin = temp.indexOf(rules[i][0]);
 
-                    String change = start.substring(0, begin + 1).concat(temp.replace(rules[i][0], rules[i][1]));
+                    String change = start.substring(0, begin + 1).concat(temp.replaceFirst(rules[i][0], rules[i][1]));
                     if (something(change, goal, step)) {
-                        System.out.println(i + " " + start.indexOf(rules[i][0]) + " " + change);
+                        output.add(i + " " + start.indexOf(rules[i][0]) + " " + change);
                         return true;
                     }
-
-                    temp = temp.substring(begin + 1);
+                    temp = temp.substring(begin + rules[i][0].length());
                 }
             }
         }
@@ -38,5 +40,8 @@ public class Main {
         int reqSteps = scan.nextInt();
 
         something(scan.next(), scan.next(), 0);
+        for (String line: output) {
+            System.out.println(line);
+        }
     }
 }
